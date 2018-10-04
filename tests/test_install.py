7b837682fbc4800c6ca46f4463f008cc3582d746
Philippe Duval <philippe@plot.ly>
2018-10-04 14:53:12 -0400

    Use pytest-dash in install test.

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,46 +1,9 @@
 import shutil
 import time
 import sys
-import pytest
-import threading
-import runpy
-
-import flask
-import requests
 
 
-@pytest.fixture
-def app():
-
-    def create_app(app_file):
-        app_file = runpy.run_path(app_file)
-        app = app_file['app']
-
-        @app.server.route('/stop')
-        def _stop_server():
-            stopper = flask.request.environ['werkzeug.server.shutdown']
-            stopper()
-            return 'stop'
-
-        def run():
-            app.scripts.config.serve_locally = True
-            app.css.config.serve_locally = True
-            app.run_server(debug=False, port=8050, threaded=True)
-
-        t = threading.Thread(target=run)
-        t.daemon = True
-        t.start()
-        time.sleep(3)
-
-        return app
-
-    yield create_app
-
-    # Stop the server in teardown
-    requests.get('http://localhost:8050/stop')
-
-
-def test_install(cookies, app, selenium):
+def test_install(cookies, dash_app, selenium):
     results = cookies.bake(extra_context={
         'project_name': 'Test Component',
         'author_name': 'test',
@@ -51,7 +14,7 @@ def test_install(cookies, app, selenium):
     # Add the generated project to the path so it can be loaded from usage.py
     sys.path.insert(0, str(results.project))
 
-    app(str(results.project.join('usage.py')))
+    dash_app(str(results.project.join('usage.py')))
 
     selenium.get('http://localhost:8050')
     time.sleep(1)
@@ -60,7 +23,8 @@ def test_install(cookies, app, selenium):
         '//div[@id="input"]/input')
     input_component.clear()
     input_component.send_keys('Hello dash component')
-    time.sleep(1)
+
+    time.sleep(2)
 
     output = selenium.find_element_by_id('output')
 
--- {{cookiecutter.project_shortname}}/tests/test_usage.py
+++ {{cookiecutter.project_shortname}}/tests/test_usage.py
@@ -1,4 +1,7 @@
-def test_render_component(dash_app, percy_snapshot):
+# Basic test for the component rendering.
+def test_render_component(dash_app, selenium):
+    # Start a dash app contained in `usage.py`
     app = dash_app('usage.py')
 
-    percy_snapshot('{{cookiecutter.project_name}} usage.py')
+    # Get the generated component with selenium
+    my_component = selenium.find_element_by_id('input')
