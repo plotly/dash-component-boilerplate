34e6b9fa548d9776eb51ac1129d4837633185df4
Philippe Duval <philippe@plot.ly>
2018-10-11 17:53:02 -0400

    Remove unused imports from demo.

--- {{cookiecutter.project_shortname}}/src/demo/App.js
+++ {{cookiecutter.project_shortname}}/src/demo/App.js
@@ -1,7 +1,5 @@
 /* eslint no-magic-numbers: 0 */
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import * as R from 'ramda';
 
 import {{cookiecutter.component_name}} from '../lib';
 
