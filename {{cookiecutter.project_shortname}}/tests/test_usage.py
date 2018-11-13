fa1dfd3f57ee22fc8067e46202736cce904bf2f9
Philippe Duval <philippe@plot.ly>
2018-11-13 14:41:56 -0500

    Better test_usage.py

--- {{cookiecutter.project_shortname}}/tests/test_usage.py
+++ {{cookiecutter.project_shortname}}/tests/test_usage.py
@@ -1,3 +1,6 @@
+import time
+
+
 # Basic test for the component rendering.
 def test_render_component(dash_app, selenium):
     # Start a dash app contained in `usage.py`
@@ -6,5 +9,19 @@ def test_render_component(dash_app, selenium):
     # and start it in a thread.
     app = dash_app('usage.py')
 
-    # Get the generated component with selenium
-    my_component = selenium.find_element_by_id('input')
+    # Get the generated component input with selenium
+    # The html input will be a children of the #input dash component
+    my_component = selenium.find_element_by_css_selector('#input > input')
+
+    # Send keys to the custom input.
+    my_component.send_keys('Hello dash')
+
+    # Sleep for the output callback to complete.
+    time.sleep(1)
+
+    # Get the output component.
+    output = selenium.find_element_by_id('output')
+
+    # assert the text has changed
+    assert output.text == 'You have entered Hello dash'
+
