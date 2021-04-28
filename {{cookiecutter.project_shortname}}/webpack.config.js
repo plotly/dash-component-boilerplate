31fcc653bee52dee7ff08ce7c9ef5fc3ad892631
xhlulu <xhlperso@gmail.com>
2021-04-28 17:35:00 -0400

    Add shared cache group to webpack config & manifest.in

--- {{cookiecutter.project_shortname}}/webpack.config.js
+++ {{cookiecutter.project_shortname}}/webpack.config.js
@@ -102,6 +102,12 @@ module.exports = (env, argv) => {
                         name(module, chunks, cacheGroupKey) {
                             return `${cacheGroupKey}-${chunks[0].name}`;
                         }
+                    },
+                    shared: {
+                        chunks: 'all',
+                        minSize: 0,
+                        minChunks: 2,
+                        name: '{{cookiecutter.project_shortname}}-shared'
                     }
                 }
             }
