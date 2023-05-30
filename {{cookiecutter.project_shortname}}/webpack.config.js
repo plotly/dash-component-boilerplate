b134e296f90d8dbd8ef8cf1bc80034e54e745335
philippe <t4rk@outlook.com>
2023-05-30 15:21:40 -0400

    Update style-loader

--- {{cookiecutter.project_shortname}}/webpack.config.js
+++ {{cookiecutter.project_shortname}}/webpack.config.js
@@ -74,9 +74,6 @@ module.exports = (env, argv) => {
                     use: [
                         {
                             loader: 'style-loader',
-                            options: {
-                                insertAt: 'top'
-                            }
                         },
                         {
                             loader: 'css-loader',
