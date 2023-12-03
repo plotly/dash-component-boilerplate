f7a55f0d53634e8a3810ddc48a54d0ec45d58942
Najeem Muhammed <najeem@gmail.com>
2023-12-03 13:28:04 +0100

    Updated setup.py to include long_description
    pypi upload will fail if long description is missing. Also, the
    content_type should also be mentioned.

--- {{cookiecutter.project_shortname}}/setup.py
+++ {{cookiecutter.project_shortname}}/setup.py
@@ -1,10 +1,11 @@
 import json
-import os
 from setuptools import setup
+from pathlib import Path
 
-
+here = Path(__file__).parent
 with open('package.json') as f:
     package = json.load(f)
+long_description = (here / 'README.md').read_text()
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
@@ -16,6 +17,8 @@ setup(
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[],
     classifiers = [
         'Framework :: Dash',
