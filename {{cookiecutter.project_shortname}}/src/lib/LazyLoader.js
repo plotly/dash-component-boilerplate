15266f78096186015370f8fd76138627398cc0fe
Neetish Singh <neetishsingh97@gmail.com>
2024-04-16 19:07:55 +0530

    Bug Fix : Lazy Loader(Async) Render issue #163
    I have resolved an issue that was related to the website not rendering properly for async due to import missing in LazyLoader cookiecutter_template

--- {{cookiecutter.project_shortname}}/src/lib/LazyLoader.js
+++ {{cookiecutter.project_shortname}}/src/lib/LazyLoader.js
@@ -1,1 +1,3 @@
+import React from 'react';
+
 export const {{cookiecutter.component_name}} = React.lazy(() => import(/* webpackChunkName: "{{cookiecutter.component_name}}" */ './fragments/{{cookiecutter.component_name}}.react'));
